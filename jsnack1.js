b664146e38ce1f02f9d265bb18ff5ea2f62b93dc
AndreaZenz <zenzero.ere@gmail.com>
2021-04-23 16:48:57 +0200

    correct

--- jsnack1.js
+++ jsnack1.js
@@ -4,81 +4,70 @@
 
 var zucchine = [
 
-    zucchina1 = {
+    {
         varieta: 1,
         peso: 20,
         lunghezza: 15
     },
 
-    zucchina2 = {
+    {
         varieta: 2,
         peso: 15,
         lunghezza: 20
     },
 
-    zucchina3 = {
+    {
         varieta: 3,
         peso: 40,
         lunghezza: 40
     },
 
-    zucchina4 = {
+    {
         varieta: 4,
         peso: 30,
         lunghezza: 2
     },
 
-    zucchina5 = {
+    {
         varieta: 5,
         peso: 12,
         lunghezza: 4
     },
 
-    zucchina6 = {
+    {
         varieta: 6,
         peso: 3,
         lunghezza: 6
     },
 
-    zucchina7 = {
+    {
         varieta: 7,
         peso: 5,
         lunghezza: 18
     },
 
-    zucchina8 = {
+    {
         varieta: 8,
         peso: 10,
         lunghezza: 6
     },
 
-    zucchina9 = {
+    {
         varieta: 9,
         peso: 18,
         lunghezza: 7
     },
 
-    zucchina10 = {
+    {
         varieta: 10,
         peso: 50,
         lunghezza: 8
     },
 ]
 
-pesoCalcolato = []
 
-for (var i = 0; i < zucchine.length; i++) {
 
-    var pesoZucchine = (zucchine[i].peso)
-    pesoCalcolato.push(pesoZucchine)
-
-}
-
-console.log(pesoCalcolato)
-
-console.log(
-    pesoCalcolato.reduce(function (acc, val) { return acc + val; }, 0)
-)
+console.log(calcoloPeso(zucchine))
 
 
 // [Snack 2]
@@ -92,17 +81,26 @@ var zucchineCorte = []
 for (var i = 0; i < zucchine.length; i++) {
     var lunghezzaZucchine = (zucchine[i].lunghezza)
 
+    // data la suddivisione in base alla lunghezza come ritorno al peso?
     if (lunghezzaZucchine <= 15) {
-        zucchineCorte.push(lunghezzaZucchine)
+        zucchineCorte.push(zucchine[i])
     } else {
-        zucchineLunghe.push(lunghezzaZucchine)
+        zucchineLunghe.push(zucchine[i])
     }
 }
+console.log(zucchineLunghe)
+console.log(calcoloPeso(zucchineLunghe))
+console.log(zucchineCorte)
+console.log(calcoloPeso(zucchineCorte))
 
 
-console.log(zucchineLunghe);
-console.log("" + zucchineLunghe.reduce(function (acc, val) { return acc + val; }, 0)
-);
-console.log(zucchineCorte);
-console.log("Il risultato del p" + zucchineCorte.reduce(function (acc, val) { return acc + val; }, 0)
-);
+// data una array di zucchine restituisce il peso totale
+
+function calcoloPeso(array){
+    var pesoCalcolato = 0
+    for (var i = 0; i < array.length; i++) {
+        var peso = (array[i].peso)
+        pesoCalcolato += peso
+    }
+    return pesoCalcolato
+}
